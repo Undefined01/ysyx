463ef2f9844372e025e6979e08c9174c52081aed
20210285-Li Han <191870085@smail.nju.edu.cn>
2021-08-05 17:47:55 +0800

    dev: Update U-type instruction tests

--- src/test/resources/basic/src/u_instr.s
+++ src/test/resources/basic/src/u_instr.s
@@ -4,8 +4,9 @@ auipc x1, 0
 auipc x2, 0
 auipc x3, 0
 auipc x4, 0
-xor x2, x2, x1
-xor x3, x3, x1
-xor x4, x4, x1
-lui x1, 0x12345
-lui x5, 0x87654
+sub x2, x2, x1
+sub x3, x3, x1
+sub x4, x4, x1
+sub x1, x1, x1
+lui x5, 0x12345
+lui x6, 0x87654
